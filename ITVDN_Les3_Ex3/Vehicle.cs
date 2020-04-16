b713bce2cf5e9ed67f666204230f3b6bb9aabd80
Bat <BataevTechno@yandex.ru>
2020-04-16 16:28:00 +0900

    Add project files.

---
+++ ITVDN_Les3_Ex3/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITVDN_Les3_Ex3
+{
+    class Vehicle
+    {
+        public double Price { get; set; }
+        public double MaxSpeed { get; set; }
+        public double Acceleration { get; set; }
+        public double YearOfProduction { get; set; }
+    }
+}
